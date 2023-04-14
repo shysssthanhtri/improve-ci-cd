0498c83132ca69c8a2d639a1aab02e951684d8ce
Tri Nguyen <shyssthanhtri@gmail.com>
2023-04-14 20:29:49 +0700

    rafactor: lint & configService

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get<ConfigService>(ConfigService);
+  const port = configService.getOrThrow('PORT');
+
+  await app.listen(port, () => {
+    Logger.log(`Listening on port ${port}: ...`);
+  });
 }
 bootstrap();
